cc193c966e25cdb9e850aa02cf320178a19573f3
Thayana Mamoré <thayanacmamore@gmail.com>
2021-04-17 15:59:29 -0300

    Deletando workflow de deploy

--- quasar.conf.js
+++ quasar.conf.js
@@ -44,7 +44,6 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      publicPath: process.env.NODE_ENV === 'production' ? '/janeraka-ryru/' : '/',
 
       // transpile: false,
 
