84557cb5c11569a46dbcd380302e950e49eaf0eb
Patrick Monteiro <eng.patrickmonteiro@gmail.com>
2021-04-27 20:07:25 -0300

    add api list

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 const axiosConfig = axios.create({
-  baseURL: 'http://mucuas.taina.net.br:8067'
+  baseURL: 'https://cors-anywhere.herokuapp.com/http://mucuas.taina.net.br:8067'
 })
 
 Vue.prototype.$axios = axiosConfig
--- src/pages/Acervo.vue
+++ src/pages/Acervo.vue
@@ -1,16 +1,47 @@
 <template>
-<q-page>
-  <Construction />
+<q-page padding>
+  <div class="row q-col-gutter-md">
+    <div class="col-3" v-for="midia in midias" :key="midia.smid">
+      <q-card class="my-card">
+        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+        <q-card-section>
+          <div class="text-h6"> {{ midia.data.titulo }}</div>
+          <div class="text-subtitle2">{{ midia.creator.username }}</div>
+          <div class="text-subtitle2">{{  midia.data.tipo }}</div>
+        </q-card-section>
+
+        <!-- <q-card-section class="q-pt-none">
+          TEXTO
+        </q-card-section> -->
+      </q-card>
+    </div>
+  </div>
 </q-page>
 </template>
 
 <script>
-import Construction from '../components/Construction'
 
 export default {
   name: 'Acervo',
-  components: {
-    Construction
+  data () {
+    return {
+      midias: []
+    }
+  },
+  mounted () {
+    this.getMidia()
+  },
+  methods: {
+    async getMidia () {
+      try {
+        const { data } = await this.$axios.get('/acervo/midia')
+        this.midias = data
+        console.log(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 }
 </script>
