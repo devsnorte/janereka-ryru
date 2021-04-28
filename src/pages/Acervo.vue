06b5428906d8619f920eaaf6e679879c2e8d635f
Thayana Mamoré <thayanacmamore@gmail.com>
2021-04-27 23:45:46 -0300

    Listando todas midias

--- src/pages/Acervo.vue
+++ src/pages/Acervo.vue
@@ -1,14 +1,23 @@
 <template>
 <q-page padding>
   <div class="row q-col-gutter-md">
-    <div class="col-3" v-for="midia in midias" :key="midia.smid">
-      <q-card class="my-card">
-        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+    <div class="col-3" v-for="info in midiaInfos" :key="info.smid">
+      <q-card>
+        <img v-if="info.data.tipo == 'imagem'" :src="`${baseUrl}${info.path}`">
+
+        <q-media-player
+          v-if="info.data.tipo == 'video'"
+          type="video"
+          :sources="[{
+            src: `${baseUrl}${info.path}`,
+          }]"
+          content-style="height: 200px;"
+        />
 
         <q-card-section>
-          <div class="text-h6"> {{ midia.data.titulo }}</div>
-          <div class="text-subtitle2">{{ midia.creator.username }}</div>
-          <div class="text-subtitle2">{{  midia.data.tipo }}</div>
+          <div class="text-h6"> {{ info.data.titulo }}</div>
+          <div class="text-subtitle2">{{ info.creator.username }}</div>
+          <div class="text-subtitle2">{{  info.data.tipo }}</div>
         </q-card-section>
 
         <!-- <q-card-section class="q-pt-none">
@@ -26,7 +35,8 @@ export default {
   name: 'Acervo',
   data () {
     return {
-      midias: []
+      baseUrl: 'http://mucuas.taina.net.br:8067/acervo/download/',
+      midiaInfos: []
     }
   },
   mounted () {
@@ -36,7 +46,7 @@ export default {
     async getMidia () {
       try {
         const { data } = await this.$axios.get('/acervo/midia')
-        this.midias = data
+        this.midiaInfos = data
         console.log(data)
       } catch (error) {
         console.error(error)
