cde4c8515ea0a9523de3a278de1f773248b2d674
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2021-08-30 22:32:46 -0300

    Internacionalização de datas

--- src/boot/i18n.js
+++ src/boot/i18n.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import messages from 'src/i18n'
+import dateTimeFormats from 'src/i18n/dateTimeFormats'
 
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
-  locale: 'en-us',
-  fallbackLocale: 'en-us',
+  locale: 'pt',
+  fallbackLocale: 'pt',
+  dateTimeFormats,
   messages
 })
 
