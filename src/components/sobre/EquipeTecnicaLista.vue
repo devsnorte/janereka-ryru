eb21ea5352ed3e310ab8ef1b1b41ee4a915f6d19
Thayana Mamoré <thayanacmamore@gmail.com>
2021-11-10 12:08:56 -0300

    Atualizando página sobre

--- src/components/sobre/EquipeTecnicaLista.vue
+++ src/components/sobre/EquipeTecnicaLista.vue
@@ -2,23 +2,23 @@
     <div class="bg-grey-12 q-py-xl q-px-xs-lg">
         <div class="q-px-xs-lg">
             <h1 class="text-h3 text-weight-bolder text-primary text-center">
-                {{ $t('about.pageTextAboutTeam') }}
+                {{ $t('about.pageTitleAboutTeam') }}
             </h1>
         </div>
 
         <div class="row constrain text-center q-pt-md-lg q-pb-md-md">
-            <div class="col-xs-12 col-md-6 q-pb-xs-xl" v-for="(info, index) in team" :key="index">
+            <div class="col-xs-12 col-sm-6 col-md-6 q-pb-xs-xl" v-for="(info, index) in teamDataforI18n" :key="index">
                 <div class="text-h6 text-weight-bolder text-primary"> {{ info.role }} </div>
                 <div class="text-subtitle1" style="white-space: pre-line;">
                     {{ `${info.name.join('\n')}` }}
                 </div>
             </div>
             <div class="col-12 q-pb-xs-xl">
-              <div class="text-h5 text-weight-bolder text-primary"> Apoio </div>
+              <div class="text-h5 text-weight-bolder text-primary"> {{$t('about.pageTitleAboutTeamRealization')}} </div>
             </div>
-            <div class="fit row justify-center">
-              <div class="col-xs-12 col-md-3 q-pb-xs-xl" v-for="(img, index) in logo" :key="index">
-                <img :src="img.url" :alt="`Logo de ${img.name}`" width="120px" />
+            <div class="fit row justify-center self-baseline">
+              <div class="col-xs-12 col-sm-3 col-md-2 q-pb-xs-xl" v-for="(img, index) in logo" :key="index">
+                <img :src="img.url" :alt="`Logo de ${img.name}`" :width="img.width" />
               </div>
             </div>
         </div>
@@ -27,48 +27,26 @@
 
 <script>
 
-const teamData = [
-  {
-    role: 'Produção Cultural e Tradução',
-    name: [
-      'Timei Assurini'
-    ]
-  },
-  {
-    role: 'Produção Cultural e Direção Executiva',
-    name: [
-      'Carla Romano Amaral'
-    ]
-  },
-  {
-    role: 'Designers',
-    name: [
-      'Gabriela Pessoa',
-      'Alice Damasceno',
-      'Jamille Santos',
-      'Yuka Yamada'
-    ]
-  },
-  {
-    role: 'Desenvolvedores',
-    name: [
-      'Alice Cruvinel Bonafé',
-      'Diego Souza',
-      'Luandro Vieira',
-      'Thayana Corrêa Mamoré',
-      'Vincenzo Tozzi'
-    ]
-  }
-]
-
 const logoImg = [
   {
     name: 'Baobáxia',
-    url: require('src/assets/LogoBBX_mono.svg')
+    url: require('src/assets/realizacao/LogoBBX_mono.svg'),
+    width: '110px'
+  },
+  {
+    name: 'Inajá Design Permacultural',
+    url: require('src/assets/realizacao/inaja.jpg'),
+    width: '110px'
+  },
+  {
+    name: 'Marytykwawara Awaete',
+    url: require('src/assets/realizacao/marytykwawara.jpg'),
+    width: '190px'
   },
   {
     name: 'Vue.js Norte',
-    url: require('src/assets/logo_VueNorte.svg')
+    url: require('src/assets/realizacao/logo_VueNorte.svg'),
+    width: '120px'
   }
 ]
 
@@ -76,8 +54,46 @@ export default {
   name: 'Equipe',
   data () {
     return {
-      team: teamData,
       logo: logoImg
+    }
+  },
+  computed: {
+    teamDataforI18n () {
+      const teamData = [
+        {
+          role: this.$t('about.pageTitleRoleTranslation'),
+          name: [
+            'Timei Assurini'
+          ]
+        },
+        {
+          role: this.$t('about.pageTitleRoleExecutiveManagement'),
+          name: [
+            'Carla Romano Amaral'
+          ]
+        },
+        {
+          role: this.$t('about.pageTitleRoleDesigner'),
+          name: [
+            'Gabriela Pessoa',
+            'Alice Damasceno',
+            'Jamille Santos',
+            'Yuka Yamada'
+          ]
+        },
+        {
+          role: this.$t('about.pageTitleRoleDev'),
+          name: [
+            'Alice Cruvinel Bonafé',
+            'Diego Souza',
+            'Luandro Vieira',
+            'Thayana Corrêa Mamoré',
+            'Vincenzo Tozzi'
+          ]
+        }
+      ]
+
+      return teamData
     }
   }
 }
--- src/components/sobre/SobreMap.vue
+++ src/components/sobre/SobreMap.vue
@@ -5,19 +5,18 @@
             <q-img src="~assets/map.svg" alt="pictures" />
           </div>
           <div class="col-xs-12 col-md-6 q-pt-xl q-pl-md-lg self-end">
-            <span class="text-weight-bold">AUTO DENOMINAÇÃO:</span> Awaete <br />
-            <span class="text-weight-bold">LÍNGUA:</span> Asurini, família Tupi-Guarani, tronco Tupi <br />
-            <span class="text-weight-bold">TERRA INDÍGENA:</span> Parque Indígena Kwatinemo <br />
-            <span class="text-weight-bold">ÁREA OFICIAL:</span> 387.834 ha. (ISA,2016) <br />
-            <span class="text-weight-bold">STATUS TERRITORIAL:</span> Demarcada e Homologada <br />
-            <span class="text-weight-bold">UNIDADE ADM. FUNAI:</span> Altamira/PA <br />
-            <span class="text-weight-bold">BIOMA:</span> Amazônia <br />
-            <span class="text-weight-bold">BACIA HIDROGRÁFICA:</span> Xingu <br />
-            <span class="text-weight-bold">COBERTURA VEGETAL:</span> Floresta Ombrófila Aberta (96.25 %) e Floresta Ombrófila Densa (3.75 %) <br />
-            <span class="text-weight-bold">ALDEIAS:</span> Itaaka e Kwatinema <br />
-            <span class="text-weight-bold">POPULAÇÃO:</span> Média de 250 PESSOAS <br />
-            <span class="text-weight-bold">ALDEIAS:</span> Kwatinema, Itaaka, Gavião, Muyryna e Janeraka. <br />
-            <span class="text-weight-bold">PRINCIPAIS SABERES TRADICIONAIS:</span> Cestaria, Adornos corporais, Cerâmica, Marcenaria, Grafismo, Construção, Instrumentos Musicais, culturas Alimentares, Medicinais e Cosmologicas.<br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutDenomination')}}:</span> Awaete <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutLanguage')}}:</span> Asurini, família Tupi-Guarani, tronco Tupi <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutIndigenousLand')}}:</span> Parque Indígena Kwatinemo <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutArea')}}:</span> 387.834 ha. (ISA,2016) <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutStatus')}}:</span> Demarcada e Homologada <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutUnit')}}:</span> Altamira/PA <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutBiome')}}:</span> Amazônia <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutBasin')}}:</span> Xingu <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutVegetal')}}:</span> Floresta Ombrófila Aberta (96.25 %) e Floresta Ombrófila Densa (3.75 %) <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutIndiginousGroup')}}:</span> Kwatinema, Itaaka, Gavião, Muyryna e Janeraka. <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutPopulation')}}:</span> Média de 250 PESSOAS <br />
+            <span class="text-weight-bold text-uppercase">{{$t('about.pageTitleAboutKnowledge')}}:</span> Cestaria, Adornos corporais, Cerâmica, Marcenaria, Grafismo, Construção, Instrumentos Musicais, culturas Alimentares, Medicinais e Cosmologicas.<br />
           </div>
         </div>
     </div>
